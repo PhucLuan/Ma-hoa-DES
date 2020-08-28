bc8efe28c247596046c6905e1e743e3b4fbd3234
PhucLuan <54372656+PhucLuan@users.noreply.github.com>
2020-08-29 01:58:37 +0700

    Add project files.

---
+++ DEMO-DES/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace DEMO_DES
+{
+    partial class DES_Encrpytion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBanRo = new System.Windows.Forms.TextBox();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnEncrpytion = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(594, 53);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(143, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(286, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DEMO MÃ HÓA DES";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(41, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nhập bản rõ M:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(41, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nhập khóa K:";
+            // 
+            // txtBanRo
+            // 
+            this.txtBanRo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBanRo.Location = new System.Drawing.Point(208, 105);
+            this.txtBanRo.Multiline = true;
+            this.txtBanRo.Name = "txtBanRo";
+            this.txtBanRo.Size = new System.Drawing.Size(221, 38);
+            this.txtBanRo.TabIndex = 3;
+            this.txtBanRo.Text = "0123456789ABCDEF";
+            this.txtBanRo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtKey
+            // 
+            this.txtKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKey.Location = new System.Drawing.Point(208, 181);
+            this.txtKey.Multiline = true;
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(221, 38);
+            this.txtKey.TabIndex = 4;
+            this.txtKey.Text = "133457799BBCDFF1";
+            this.txtKey.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(41, 315);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Kết quả:";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(208, 302);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(221, 38);
+            this.txtResult.TabIndex = 6;
+            // 
+            // btnEncrpytion
+            // 
+            this.btnEncrpytion.Location = new System.Drawing.Point(272, 245);
+            this.btnEncrpytion.Name = "btnEncrpytion";
+            this.btnEncrpytion.Size = new System.Drawing.Size(91, 33);
+            this.btnEncrpytion.TabIndex = 7;
+            this.btnEncrpytion.Text = "Mã hóa";
+            this.btnEncrpytion.UseVisualStyleBackColor = true;
+            this.btnEncrpytion.Click += new System.EventHandler(this.btnEncrpytion_Click);
+            // 
+            // DES_Encrpytion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 450);
+            this.Controls.Add(this.btnEncrpytion);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.txtBanRo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Name = "DES_Encrpytion";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DES_Encrpytion";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBanRo;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnEncrpytion;
+    }
+}
+
---
+++ DEMO-DES/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaHoaDES;
+
+
+namespace DEMO_DES
+{
+    public partial class DES_Encrpytion : Form
+    {
+        public DES_Encrpytion()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnEncrpytion_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtBanRo.Text) == false && string.IsNullOrEmpty(txtKey.Text) == false)
+            {
+                txtResult.Text = DesLibrary.EncrpytionDES(txtKey.Text.ToString(), txtBanRo.Text.ToString());
+               
+            }
+            else
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin");
+        }
+    }
+}
